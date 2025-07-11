596b37932d9fa294e7d2cdd99c6388d34aade695
Wafi uddin <fatimatr00040@gmail.com>
2025-07-11 09:03:56 +0500

    Add files via upload

---
+++ 01-basics/06-strings.js
@@ -0,0 +1,24 @@
+const name = "hitash"
+const repoCount = 45;
+
+
+// console.log(name + repoCount + "value");//we can concanate strings like this but abother method we have avaiable.
+
+// another method
+
+console.log(`hello my name is ${name} and my repo is ${repoCount}all about me`);
+
+// another method of adding string 
+const gameName = new String('wafi-trrrr');
+
+console.log(gameName[0]); //show us the letter on its index number
+console.log(gameName.__proto__);
+
+console.log(gameName.length); //it will show us the lenght is string
+console.log(gameName.toUpperCase()) // it will convet to uppercase
+console.log(gameName.charAt(2)); //we have to put the number and it show us the charater 
+console.log(gameName.indexOf("t")); //just we have to write the charater it will tell us the index os that number.
+
+
+
+
